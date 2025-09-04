68c90696f2db68e5e2f2ee060fe1952cda9bfc67
AHMED123457788 <103061659+AHMED123457788@users.noreply.github.com>
2025-09-04 13:08:04 +0100

    app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const videos = [
     id: 'vid3',
     title: 'New Video 2',
     desc: 'الفيديو الثاني',
-    src: 'https://drive.google.com/file/d/1Q7eu8Oar72TiU7epXxOGxc6H8m7Ymk17/view?usp=sharing'
+    src: 'https://drive.google.com/file/d/1Q7eu8Oar72TiU7epXxOGxc6H8m7Ymk17/view?t=1'
   }
   
 ];
