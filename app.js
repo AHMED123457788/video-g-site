b75c86eab7dc1b5e9211cfe8df98fba0fa2ada64
AHMED123457788 <103061659+AHMED123457788@users.noreply.github.com>
2025-09-04 16:31:20 +0100

    app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function enterSite() {
 }
 
 // --- Old Video Posts ---
-const videoSrc = "https://drive.google.com/file/d/1FAsokB11cSc4Fk6b4fVSSZOv2fbZxlkR/view?usp=sharing"; // ضع رابط Dropbox مباشر هنا
+const videoSrc = "https://github.com/AHMED123457788/video-g-site/blob/main/17569777605Nelh5Lcjh22TQ.mp4"; // ضع رابط Dropbox مباشر هنا
 const oldVideoContainer = document.getElementById("oldVideoPosts");
 const oldVideoLinks = document.getElementById("oldVideoLinks");
 
