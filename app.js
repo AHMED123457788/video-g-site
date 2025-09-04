7b7de8ec7c065226a7f7f611cdffc6bc76bdba59
AHMED123457788 <103061659+AHMED123457788@users.noreply.github.com>
2025-09-04 16:15:58 +0100

    app.js

--- app.js
+++ app.js
@@ -2,16 +2,6 @@
 const ageGate = document.getElementById("ageGate");
 const mainContent = document.getElementById("mainContent");
 
-// إظهار Age Gate عند فتح الصفحة
-window.onload = function() {
-  ageGate.style.display = "block";
-};
-
-function enterSite() {
-  ageGate.style.display = "none";
-  mainContent.style.display = "block";
-}
-
 // --- Old Video Posts ---
 const videoSrc = "https://www.dropbox.com/scl/fi/ivsbqsdqamoj2y0laqz7v/17569777605Nelh5Lcjh22TQ.mp4?rlkey=nxi7raqp6s1uvzj31czwxq9o6&st=vqcmgd5j&dl=0";
 const oldVideoContainer = document.getElementById("oldVideoPosts");
